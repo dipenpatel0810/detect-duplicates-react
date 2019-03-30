0c032563650f482e470a554e3fe077b838e5e546
Dipen Patel <patel.dip@husky.neu.edu>
2019-03-30 19:39:10 -0400

    Modified the URL form localhost to the one on Heroku

--- src/services/DataService.js
+++ src/services/DataService.js
@@ -1,7 +1,7 @@
-const normalDuplicate = 'http://localhost:8080/api/normal/duplicate';
-const normalUnique = 'http://localhost:8080/api/normal/unique';
-const advancedDuplicate = 'http://localhost:8080/api/advanced/duplicate';
-const advancedUnique = 'http://localhost:8080/api/advanced/unique';
+const normalDuplicate = 'https://detect-duplicates-server.herokuapp.com/api/normal/duplicate';
+const normalUnique = 'https://detect-duplicates-server.herokuapp.com/api/normal/unique';
+const advancedDuplicate = 'https://detect-duplicates-server.herokuapp.com/api/advanced/duplicate';
+const advancedUnique = 'https://detect-duplicates-server.herokuapp.com/api/advanced/unique';
 
 export default class DataService {
     findDuplicateForNormal = () =>
